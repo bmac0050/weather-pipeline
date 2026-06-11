ad9e61545670f0ab1a226f55f4b03aaeeecbdcb9
BMac <bmac@MBP.local>
2026-06-10 22:37:29 -0400

    Add multi-city weather extraction

--- scripts/extract_weather.py
+++ scripts/extract_weather.py
@@ -1,37 +1,48 @@
 import requests
 import pandas as pd
 from datetime import datetime
-
-
+from time import sleep
 from config.settings import OPENWEATHER_API_KEY
 
-city = "Cincinnati, OH, US"
+datafile = "data/raw/weather_data.csv"
+cities = ["Cincinnati, OH, US", "Dallas, TX, US", "Wilmington, NC, US"]
+url = 'https://api.openweathermap.org/data/2.5/weather'
+weather_rows = []
 
-url = (
-    "https://api.openweathermap.org/data/2.5/weather"
-    f"?q={city}"
-    f"&appid={OPENWEATHER_API_KEY}"
-    "&units=imperial"
-)
+for city in cities:
+    try:
+        params = {
+            "q": city,
+            "units": "imperial",
+            "appid": OPENWEATHER_API_KEY
+        }
+        print(f'Retrieving weather data for {city}...')
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an error for HTTP errors
+        print(f"Status for {city}: {response.status_code}")
 
-response = requests.get(url)
+        data = response.json()
 
-print("Status:", response.status_code)
+        weather_rows.append({
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'humidity': data['main']['humidity'],
+            'weather_timestamp': datetime.fromtimestamp(data['dt']),
+            'retrieved_at': datetime.now()
+        })
 
-data = response.json()
+        sleep(3.0)
 
-weather_data = [{
-    "city": data["name"],
-    "temperature": data["main"]["temp"],
-    "humidity": data["main"]["humidity"],
-    "weather_timestamp": datetime.fromtimestamp(data["dt"]),
-    "retrieved_at": datetime.now()
-}]
+    except requests.RequestException as e:
+        print(f"Error fetching weather data for {city}: {e}")
+        continue
 
-df = pd.DataFrame(weather_data)
+
+df = pd.DataFrame(weather_rows)
 
 # Display the DataFrame
-# print(df)
+print(df)
 
 # Write the DataFrame to a CSV file
-df.to_csv("data/raw/weather_data.csv", index=False)
+print(f"Retrieved {len(df)} weather records")
+df.to_csv(datafile, index=False)
